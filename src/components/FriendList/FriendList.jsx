f3a46ccbf9423df3a05dc1967633036c38007b63
JarPr <131611487+JarPr@users.noreply.github.com>
2024-08-10 14:11:35 +0200

    basis

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,16 @@
+import FriendListItem from '../FriendListItem/FriendListItem'
+
+
+export default function FriendList({friends}) {
+    return (
+       <ul>
+            {friends.map(item => {
+                return (
+                    <li key={item.id}>
+                        <FriendListItem friend={item} />
+                    </li>
+                );
+    })}
+</ul> 
+    )
+}
